5795c7e3a8e7e4b778205048deb07aab348c184f
pankaj-404 <pankajbhayre@gmial.com>
2023-07-20 22:49:59 +0530

    Added admin backend-end route

--- server/routes/management.js
+++ server/routes/management.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { grtAdmins } from "../controller/management.js";
 
 const router = express.Router();
+
+router.get("/admins", grtAdmins);
 
 export default router;
