cbb17bbae5f69c1637176d02c377ec6078bb146d
pankaj-404 <pankajbhayre@gmial.com>
2023-07-20 23:40:33 +0530

    Added performance controller

--- server/controller/management.js
+++ server/controller/management.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 export const grtAdmins = async (req, res) => {
   try {
@@ -7,6 +8,39 @@ export const grtAdmins = async (req, res) => {
       "-password -createdAt -updatedAt -transactions"
     );
     res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const grtUserPerformance = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const userWithStats = await User.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(id) } },
+      {
+        $lookup: {
+          from: "affiliatestats",
+          localField: "_id",
+          foreignField: "userId",
+          as: "affiliateStats",
+        },
+      },
+      { $unwind: "$affiliateStats" },
+    ]);
+
+    const saleTransactions = await Promise.all(
+      userWithStats[0].affiliateStats.affiliateSales.map((id) => {
+        return Transaction.findById(id);
+      })
+    );
+    const filteredSaleTransactions = saleTransactions.filter(
+      (transaction) => transaction !== null
+    );
+
+    res
+      .status(200)
+      .json({ user: userWithStats[0], sales: filteredSaleTransactions });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
