367c6a6b443a5328889448f4443d756e82a18408
pankaj-404 <pankajbhayre@gmial.com>
2023-07-20 13:41:50 +0530

    Added rout, controller for geography

--- server/routes/client.js
+++ server/routes/client.js
@@ -3,6 +3,7 @@ import {
   getCustomers,
   getProduct,
   getTransactions,
+  getGeography,
 } from "../controller/client.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/products", getProduct);
 router.get("/customers", getCustomers);
 router.get("/transactions", getTransactions);
+router.get("/geography", getGeography);
 
 export default router;
