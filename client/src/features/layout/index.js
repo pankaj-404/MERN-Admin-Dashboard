b6dc34e7daa386c80e935a5c541afaaeb0fc2555
pankaj-404 <pankajbhayre@gmial.com>
2023-07-20 12:51:59 +0530

    Added layout page

---
+++ client/src/features/layout/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "state/apis";
+
+function Layout() {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data: userData } = useGetUserQuery(userId);
+
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        user={userData || {}}
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          user={userData || {}}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+}
+
+export default Layout;
