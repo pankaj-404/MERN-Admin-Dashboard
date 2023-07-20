4034f8dc6a1053e1c007976b613102631c538dbe
pankaj-404 <pankajbhayre@gmial.com>
2023-07-20 23:40:05 +0530

    Added performance backend-end route

--- server/routes/management.js
+++ server/routes/management.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { grtAdmins } from "../controller/management.js";
+import { grtAdmins, grtUserPerformance } from "../controller/management.js";
 
 const router = express.Router();
 
 router.get("/admins", grtAdmins);
+router.get("/performance/:id", grtUserPerformance);
 
 export default router;
